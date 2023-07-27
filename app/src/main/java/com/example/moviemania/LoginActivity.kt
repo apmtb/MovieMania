f14b8832f84a64d8feb154b545221edd4c83bf5e
ypmmt <140507851+ypmmt@users.noreply.github.com>
2023-07-27 19:46:02 +0530

    Initial commit

---
+++ app/src/main/java/com/example/moviemania/LoginActivity.kt
@@ -0,0 +1,15 @@
+package com.example.moviemania
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login) // This sets the login.xml layout as the content view
+    }
+
+    // Add the login logic here
+    // For example, handling login button clicks, authentication, etc.
+}
---
+++ app/src/main/java/com/example/moviemania/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.moviemania
+
+import android.net.Uri
+import android.os.Bundle
+import android.widget.VideoView
+import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.content.SharedPreferences
+import androidx.preference.PreferenceManager
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var videoView: VideoView
+    private lateinit var sharedPreferences: SharedPreferences
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        val firstTime = sharedPreferences.getBoolean("firstTime", true)
+
+        if(firstTime) {
+            sharedPreferences.edit().putBoolean("firstTime", false).apply()
+            setContentView(R.layout.activity_main)
+
+            videoView = findViewById(R.id.videoView)
+
+            // Set the video path from the raw folder
+            val videoPath = "android.resource://" + packageName + "/" + R.raw.start
+            videoView.setVideoURI(Uri.parse(videoPath))
+
+            // Start playing the video
+            videoView.setZOrderOnTop(true)
+            videoView.start()
+
+            // Set a listener to navigate to the next screen after video playback
+            videoView.setOnCompletionListener {
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        } else {
+            setContentView(R.layout.login)
+        }
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        sharedPreferences.edit().putBoolean("firstTime", true).apply()
+    }
+}
