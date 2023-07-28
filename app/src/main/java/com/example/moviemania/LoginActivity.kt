55eb746038c387e6ba60c3383f25599be20e18ff
ypmmt <140507851+ypmmt@users.noreply.github.com>
2023-07-28 10:29:47 +0530

    Y

--- app/src/main/java/com/example/moviemania/LoginActivity.kt
+++ app/src/main/java/com/example/moviemania/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.example.moviemania
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
@@ -8,6 +10,11 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login) // This sets the login.xml layout as the content view
+        val signUpText = findViewById<TextView>(R.id.SignUp)
+        signUpText.setOnClickListener {
+            val intent = Intent(this, RegistrationActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     // Add the login logic here
