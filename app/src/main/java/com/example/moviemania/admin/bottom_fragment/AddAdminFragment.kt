92a63873657e60c4ae102ef06133d34832318b08
ypmmt <140507851+ypmmt@users.noreply.github.com>
2023-08-10 14:53:01 +0530

    Y

--- app/src/main/java/com/example/moviemania/admin/bottom_fragment/AddAdminFragment.kt
+++ app/src/main/java/com/example/moviemania/admin/bottom_fragment/AddAdminFragment.kt
@@ -1,11 +1,19 @@
 package com.example.moviemania.admin.bottom_fragment
 
+import android.net.Uri
 import android.os.Bundle
+import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import android.widget.VideoView
+import androidx.core.content.ContextCompat
 import com.example.moviemania.R
+import com.google.firebase.firestore.FirebaseFirestore
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -22,6 +30,10 @@ class AddAdminFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    private val db = FirebaseFirestore.getInstance()
+    private lateinit var videoView: VideoView
+    private lateinit var frameLayout: View
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -34,8 +46,53 @@ class AddAdminFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view = inflater.inflate(R.layout.admin_add_admin, container, false)
+        videoView = view.findViewById(R.id.videoViewLoadingCircleNA)
+        frameLayout = view.findViewById(R.id.frameLayoutNA)
+        val addAdminBTN = view.findViewById<Button>(R.id.addnewadmin)
+        addAdminBTN.setOnClickListener {
+            val email = view.findViewById<EditText>(R.id.emailnewadmin).text.toString().trim()
+            if (validateForm(email)) {
+                frameLayout.visibility = View.VISIBLE
+                videoView.setOnPreparedListener {
+                    it.isLooping = true
+                }
+                val videoPath = "android.resource://" + requireContext().packageName + "/" + R.raw.circle_loading
+
+                videoView.setVideoURI(Uri.parse(videoPath))
+                videoView.setZOrderOnTop(true)
+
+                videoView.start()
+                val adminMailCollection = db.collection("AdminUsers")
+
+                adminMailCollection.whereEqualTo("email", email)
+                    .get()
+                    .addOnSuccessListener { querySnapshot ->
+                        frameLayout.visibility = View.GONE
+                        videoView.stopPlayback()
+                        if (querySnapshot.isEmpty) {
+                            // Email does not exist, add it to the collection
+                            val data = hashMapOf("email" to email)
+                            adminMailCollection.add(data)
+                                .addOnSuccessListener {
+                                    showToast("Admin email added successfully.")
+                                }
+                                .addOnFailureListener {
+                                    showToast("Error adding email.")
+                                }
+                        } else {
+                            showToast("Email already exists.")
+                        }
+                    }
+                    .addOnFailureListener {
+                        frameLayout.visibility = View.GONE
+                        videoView.stopPlayback()
+                        showToast("Error checking email.")
+                    }
+            }
+        }
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.admin_add_admin, container, false)
+        return view
     }
 
     companion object {
@@ -56,5 +113,22 @@ class AddAdminFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    private fun validateForm(email: String): Boolean {
+        val icon = ContextCompat.getDrawable(requireContext(), R.drawable.ic_custom_error)
+        icon?.setBounds(0, 0, icon.intrinsicWidth, icon.intrinsicHeight)
+
+        if (email.isEmpty()) {
+            requireView().findViewById<EditText>(R.id.emailnewadmin).setError("Email is required.", icon)
+            return false
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            requireView().findViewById<EditText>(R.id.emailnewadmin).setError("Invalid email address.", icon)
+            return false
+        }
+        return true
+    }
+    private fun showToast(message: String){
+        Toast.makeText(requireContext(),message,Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/example/moviemania/admin/nav_fragment/HomeFragment.kt
+++ app/src/main/java/com/example/moviemania/admin/nav_fragment/HomeFragment.kt
@@ -5,12 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import com.example.moviemania.R
 import com.example.moviemania.admin.bottom_fragment.AddAdminFragment
 import com.example.moviemania.admin.bottom_fragment.CartFragment
 import com.example.moviemania.admin.bottom_fragment.MoviesFragment
-import com.example.moviemania.admin.bottom_fragment.TheatorsFragment
+import com.example.moviemania.admin.bottom_fragment.TheatersFragment
 import com.example.moviemania.admin.bottom_fragment.NotificationFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -32,9 +31,9 @@ class HomeFragment : Fragment() {
                     replaceFragment(MoviesFragment())
                     activity?.title = "Movies"
                 }
-                R.id.bottom_theators -> {
-                    replaceFragment(TheatorsFragment())
-                    activity?.title = "Theators"
+                R.id.bottom_theaters -> {
+                    replaceFragment(TheatersFragment())
+                    activity?.title = "Theaters"
                 }
                 R.id.bottom_notification -> {
                     replaceFragment(NotificationFragment())
